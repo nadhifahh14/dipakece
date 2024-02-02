5d04fd1f90cddfa1688cb9416b8306bbc9582a37
nadhifahh14 <158541647+nadhifahh14@users.noreply.github.com>
2024-02-02 11:31:02 +0700

    Add files via upload

---
+++ data/ADMINISTRASI_LN_25K_1.js
@@ -0,0 +1,1 @@
+var json_ADMINISTRASI_LN_25K_1 = {"type":"FeatureCollection","name":"ADMINISTRASI_LN_25K_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
